3c0a8e4dc46dd0e4d66e5f2c07ac08d93173749d
Arshad Khan <khanarshad0003@outlook.com>
2023-06-05 15:28:19 +0530

    Update Practical 10.cpp

--- DSAL/Practical 10.cpp
+++ DSAL/Practical 10.cpp
@@ -1,90 +1,84 @@
-// Author : Arshad Khan
-// Instagram : khan0003.py
-
 #include<iostream>
 using namespace std;
-class hp
-{
-   int heap[20],heap1[20],x,n1,i;
-   public:
-   hp()
-   { heap[0]=0;  heap1[0]=0;
-   } 
-   void getdata();
-   void insert1(int heap[],int);
-   void upadjust1(int heap[],int);
-   void insert2(int heap1[],int);
-   void upadjust2(int heap1[],int);
-   void minmax();
-};
-void hp::getdata()
-{  
-   cout<<"\n enter the no. of students";
-   cin>>n1;
-   cout<<"\n enter the marks";
-   for(i=0;i<n1;i++)
-   {   cin>>x;
-       insert1(heap,x);
-       insert2(heap1,x);
-   }
-}
-void hp::insert1(int heap[20],int x)
-{
-   int n;
-   n=heap[0]; 
-   heap[n+1]=x;
-   heap[0]=n+1;
-  
-   upadjust1(heap,n+1);
-}
-void hp::upadjust1(int heap[20],int i)
-{
-    int temp;
-    while(i>1&&heap[i]>heap[i/2])
-    {
-       temp=heap[i];
-       heap[i]=heap[i/2];
-       heap[i/2]=temp;
-       i=i/2;
-    }
-}
-void hp::insert2(int heap1[20],int x)
-{
-   int n;
-   n=heap1[0]; 
-   heap1[n+1]=x;
-   heap1[0]=n+1;
-  
-   upadjust2(heap1,n+1);
-}
-void hp::upadjust2(int heap1[20],int i)
-{
-    int temp1;
-    while(i>1&&heap1[i]<heap1[i/2])
-    {
-       temp1=heap1[i];
-       heap1[i]=heap1[i/2];
-       heap1[i/2]=temp1;
-       i=i/2;
-    }
-}
-void hp::minmax()
-{
-   cout<<"\n max marks"<<heap[1];
-   cout<<"\n";
-   for(i=1;i<=n1;i++)
-   {   cout<<"\n"<<heap[i];  }
-   cout<<"\n min marks"<<heap1[1];
-   cout<<"\n";
-   for(i=1;i<=n1;i++)
-   {   cout<<"\n"<<heap1[i];  }
-}
-int main()
-{  
-  hp h;
-  h.getdata();
-  h.minmax();
-  return 0;
-}
 
-// ©arshad_khan
+class Heap {
+public:
+	int minHeap[20];
+	int maxHeap[20];
+	int n;
+
+	Heap() {
+		minHeap[0] = 0;
+		maxHeap[0] = 0;
+	}
+
+	void getData() {
+		int value;
+		cout << "Enter no. of students : ";
+		cin >> n;
+		cout << endl << "Enter marks of students : " << endl;
+		for (int i = 1; i <= n; i++) {
+			cin >> value;
+			insertMin(minHeap, value);
+			insertMax(maxHeap, value);
+		}
+	}
+
+	void insertMax(int heap[], int x) {
+		int n;
+		n = maxHeap[0];
+		maxHeap[n + 1] = x;
+		maxHeap[0] = n + 1;
+
+		updateMaxHeap(maxHeap, n + 1);
+	}
+
+	void updateMaxHeap(int heap[], int x) {
+		int temp;
+		while (x > 1 && maxHeap[x] > maxHeap[x / 2]) {
+			temp = maxHeap[x];
+			maxHeap[x] = maxHeap[x / 2];
+			maxHeap[x / 2] = temp;
+			x = x / 2;
+		}
+	}
+
+	void insertMin(int heap[], int x) {
+		int n;
+		n = minHeap[0];
+		minHeap[n + 1] = x;
+		minHeap[0] = n + 1;
+
+		updateMinHeap(minHeap, n + 1);
+
+	}
+
+	void updateMinHeap(int heap[], int x) {
+		int temp;
+		while (x > 1 && minHeap[x] < minHeap[x / 2]) {
+			temp = minHeap[x];
+			minHeap[x] = minHeap[x / 2];
+			minHeap[x / 2] = temp;
+			x = x / 2;
+		}
+	}
+
+	int minMaxValue() {
+		cout << endl;
+		cout << endl << "Minimum Value : " << minHeap[1];
+		cout << endl << "Maximum Value : " << maxHeap[1];
+	}
+};
+
+int main() {
+	Heap h;
+	h.getData();
+	h.minMaxValue();
+	cout << endl;
+	cout << "Max Heap Values : ";
+	for (int i = 1; i <= h.n; i++) {
+		cout << h.maxHeap[i];
+		cout<<" - ";
+	}
+	return 0;
+}
